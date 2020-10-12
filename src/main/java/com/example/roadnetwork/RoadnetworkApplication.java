7b951f0574e5667c7c02ce946f09198608c84a0e
HariKrishnaReddi <harikrishna.reddi@gmail.com>
2020-10-12 00:17:06 -0400

    Initial Commit

---
+++ src/main/java/com/example/roadnetwork/RoadnetworkApplication.java
@@ -0,0 +1,13 @@
+package com.example.roadnetwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoadnetworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoadnetworkApplication.class, args);
+	}
+
+}
